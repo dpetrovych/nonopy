ed8d26f2e8ae1197e93c0d128677a3e278e52a2a
dpetrovych <dimati93@gmail.com>
2020-12-10 12:53:15 +0200

    add parser tests

--- nonopy/parser.py
+++ nonopy/parser.py
@@ -3,42 +3,72 @@ from itertools import takewhile
 from nonopy.nonogram import Nonogram, Nonotask
 
 class Parser():
-    def parse(self, lines):
-        """
-        Parses string in .non format to a structured information
-        """
-        ilines = iter(lines)
+    """Parses .non files, see examples in data/ folder"""
 
-        def parse_multiline(iterator):
-            rows = []
-            while (line := next(iterator, None)) is not None and line[0].isdigit():
-                row_clues = line.rstrip('\n').split(',')
-                rows.append([int(row_clue) for row_clue in row_clues])
-            else: return rows, line
+    def parse(self, lines):
+        """Parses lines of a file in .non format to a structured information
+
+        Args:
+            lines (list[str]): a file represented as a list of lines
+
+        Raises:
+            ValueError: length of column or row array does not match width or height respectively
+
+        Returns:
+            Nonogram: object representing a puzzle
+        """
         
+        ilines = iter(lines)
         descriptor = {}
         while (line := next(ilines, None)):
             if line in {'rows\n', 'columns\n'}:
-                descriptor[line.rstrip('\n')], line = parse_multiline(ilines)
+                descriptor[line.rstrip('\n')], line = self.__parse_multiline(ilines)
             
+            if line is None:
+                break
+
             if line == '\n':
                 continue
 
             [key, value] = line.split(' ', 1)
             descriptor[key] = value.strip('"\n')
 
+        self.__check_required_attributes(descriptor, ['height', 'width', 'rows', 'columns', 'goal'])
+
         height, width = int(descriptor['height']), int(descriptor['width'])
         rows, columns = descriptor['rows'], descriptor['columns']
 
         if (rows_len := len(rows)) != height:
-            raise ValueError(f'rows length {rows_len} shoule be {height}')
+            raise ValueError(f'rows length shoule be {height} (found {rows_len})')
 
         if (columns_len := len(columns)) != width:
-            raise ValueError(f'columns length {columns_len} shoule be {width}')
+            raise ValueError(f'columns length shoule be {width} (found {columns_len})')
 
         task = Nonotask(rows = rows, columns = columns)
 
         goal = descriptor['goal']
-        goal_formated = ''.join((goal[i * width: (i+1) * width] + '\n' for i in range(height)))
+        if len(goal) != height * width:
+            raise ValueError(f'goal length should be {height * width}')
 
+        goal_formated = ''.join((goal[i * width: (i+1) * width] + '\n' for i in range(height)))
         return Nonogram(task, goal_formated)
+
+    def __parse_multiline(self, iterator):
+            """Parses a multiline value like as rows and columns tasks
+
+            Args:
+                iterator (iter): file lines iterator
+
+            Returns:
+                (list[list[int]], line): list of tasks and a current line
+            """
+            rows = []
+            while (line := next(iterator, None)) is not None and line[0].isdigit():
+                row_clues = line.rstrip('\n').split(',')
+                rows.append([int(row_clue) for row_clue in row_clues])
+            else: return rows, line
+
+    def __check_required_attributes(self, descriptor, required_keys):
+        for key in required_keys:
+            if key not in descriptor:
+                raise ValueError(f'{key} is required')
---
+++ tests/parser.py
@@ -0,0 +1,131 @@
+from nonopy.parser import Parser
+from tests.testcase import TestCase
+
+
+class ParserShould(TestCase):
+    def test_empty_file(self):
+        parser = Parser()
+
+        with self.assertRaises(ValueError) as cm:
+            parser.parse([])
+
+        self.assertEqual(str(cm.exception), 'height is required')
+
+    def test_min_valid_file(self):
+        parser = Parser()
+        content = '''
+width 0
+height 0
+goal ""
+rows
+
+columns
+
+'''
+        nonogram = parser.parse(content.splitlines(True))
+
+        self.assertEqual(nonogram.task.rows, [])
+        self.assertEqual(nonogram.task.columns, [])
+        self.assertEqual(nonogram.goal, '')
+
+    def test_task_not_validated(self):
+        parser = Parser()
+        content = '''
+width 2
+height 2
+goal "0000"
+rows
+1,1,1,1,1
+100500
+
+columns
+1,2,3,4,5
+0
+'''
+        nonogram = parser.parse(content.splitlines(True))
+
+        self.assertListEqual(nonogram.task.rows, [[1, 1, 1, 1, 1], [100500]])
+        self.assertListEqual(nonogram.task.columns, [[1, 2, 3, 4, 5], [0]])
+        self.assertEqual(nonogram.goal, '00\n00\n')
+
+
+    def test_invalid_n_rows(self):
+        parser = Parser()
+        content = '''
+width 3
+height 2
+goal "101101"
+rows
+1,1
+
+columns
+2
+0
+2
+'''
+        with self.assertRaises(ValueError) as cm:
+            parser.parse(content.splitlines(True))
+
+        self.assertEqual(str(cm.exception), 'rows length shoule be 2 (found 1)')
+
+    def test_invalid_n_columns(self):
+        parser = Parser()
+        content = '''
+width 3
+height 2
+goal "101101"
+rows
+1,1
+1,1
+
+columns
+2
+0
+2
+0
+'''
+        with self.assertRaises(ValueError) as cm:
+            parser.parse(content.splitlines(True))
+
+        self.assertEqual(str(cm.exception), 'columns length shoule be 3 (found 4)')
+
+
+
+    def test_goal_is_validated_for_length(self):
+        parser = Parser()
+        content = '''
+width 2
+height 2
+goal "111111"
+rows
+2
+2
+
+columns
+2
+2
+'''
+        with self.assertRaises(ValueError) as cm:
+            parser.parse(content.splitlines(True))
+
+        self.assertEqual(str(cm.exception), 'goal length should be 4')
+
+    
+    def test_goal_has_invalid_chars(self):
+        parser = Parser()
+        content = '''
+width 2
+height 2
+goal "abcd"
+rows
+2
+2
+
+columns
+2
+2
+'''
+        with self.assertRaises(ValueError) as cm:
+            parser.parse(content.splitlines(True))
+
+        self.assertEqual(str(cm.exception), 'goal should be a string of 2 rows, [01]{2} characters each, end with an empty line')
